ec55989b2cff17c31a8d6a491495e142193b9360
Ali Malluh <109945441+AliMalluh@users.noreply.github.com>
2024-08-10 18:55:16 +0300

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Ali-Malluh on 29/10/2023.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
